f425e21abfad82c7ca83a685e7e207a4c47639ec
JoonYoung <moter2588@gmail.com>
2018-07-02 20:01:00 +0900

    2018 07 02 Post scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
   
-  #루트 경로
-  root 'home#index'
 
-  devise_for :users
+  #posts 경로
+  resources :posts
+  
+  #루트 경로
+  root :to => "home#index"
+
+  #devise 경로
+  devise_for :users,
+
+  #경로설정 costomizing
+  path: 'users',
+  path_name: {sign_in: 'login', sign_out: 'logout'}
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_30_093505) do
+ActiveRecord::Schema.define(version: 2018_07_02_094126) do
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", default: "", null: false
