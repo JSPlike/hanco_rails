c6f76b251eb3c6b6e8b2eb0f2158b638b809b09a
JoonYoung <moter2588@gmail.com>
2018-07-08 20:46:08 +0900

    2017 07 08 restration

--- db/migrate/20180630093505_devise_create_users.rb
+++ db/migrate/20180630093505_devise_create_users.rb
@@ -5,8 +5,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
 
       ## costomizing
-
       t.string :name,               null: false, default: ""
+      
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
