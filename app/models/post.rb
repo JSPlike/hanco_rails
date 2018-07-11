0297ca048493c63741aa16d84c5e23e697a013ea
JoonYoung <moter2588@gmail.com>
2018-07-11 14:48:27 +0900

    model mount

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
+	mount_uploader :image, AvatarUploader
+
 	belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
+  mount_uploader :avatar, AvatarUploader
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
 
-  has_many :posts       
+  has_many :posts
 end
