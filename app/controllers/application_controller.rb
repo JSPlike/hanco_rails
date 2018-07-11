2ce79b40327621c828215794adba4db92a91599e
JoonYoung <moter2588@gmail.com>
2018-07-11 14:34:12 +0900

    2017 07 11 avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::Base
 	## devise에서 새로추가한 컬럼의 파라미터를 받을때 사용한다.
-
+	protect_from_forgery with: :exception
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
 	protected
 
 	def configure_permitted_parameters
     	devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    	devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    	devise_parameter_sanitizer.permit(:account_update, keys: [:name, :avatar])
   end
 end
