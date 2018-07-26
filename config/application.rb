c3466a645ab6fb4b0ed5a4f5810927a6579f9bb9
JoonYoung <moter2588@gmail.com>
2018-07-26 14:27:21 +0900

    2018 07 26

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require 'rails/all'
 require 'carrierwave'
 require 'carrierwave/orm/activerecord'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -15,6 +16,9 @@ module HancoRails
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    #서울 시간대로 조정
+    config.time_zone = 'Seoul'
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
